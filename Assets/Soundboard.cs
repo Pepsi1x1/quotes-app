b732d135c88be500afd07235413c50a1a5fcd754
Tom H <tom@codersforcorbyn.com>
2017-04-24 13:49:58 +0100

    Initial Commit
    Single image and sound file - will be replaced as soon as actual assets
    available

---
+++ Assets/Soundboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class Soundboard : MonoBehaviour {
+
+    public AudioSource Player;
+    public AudioClip[] Clips;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    System.Random rand = new System.Random();
+
+    public void RandomSound()
+    {
+        Player.Stop();
+        Player.clip = Clips[rand.Next(0, Clips.Length - 1)];
+        Player.Play();
+    }
+}
